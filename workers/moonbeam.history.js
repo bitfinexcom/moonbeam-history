e8ee53f793d436fedfa3ea670fe83590127b6579
Robert Kowalski <rok@kowalski.gd>
2019-03-26 23:30:05 +0100

    candles/trades: make trade symbol prefix configurable

--- workers/moonbeam.history.js
+++ workers/moonbeam.history.js
@@ -16,7 +16,7 @@ class MoonbeamHistory {
     const trade = {
       id: id,
       pair: pair,
-      symbol: 't' + pair,
+      symbol: this.conf.tradeSymbolPrefix + pair,
       t: t / 1000,
       price: price,
       amount: amount,
